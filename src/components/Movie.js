97032437bd1459b5b21dba5b527b6c829dba5513
Arunkumar Palaniappan <11667975+arunkumarpalaniappan@users.noreply.github.com>
2018-05-11 09:27:43 +0530

    Movies info page Updated

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -15,6 +15,7 @@ import _ from 'lodash';
 import { CircularProgress } from 'material-ui/Progress';
 import Fade from 'material-ui/transitions/Fade';
 import {Link} from 'react-router-dom';
+import StarRatingComponent from 'react-star-rating-component';
 class Movie extends React.Component {
     constructor(props) {
         super(props);
@@ -33,6 +34,46 @@ class Movie extends React.Component {
             <div className={"movie-container"}>
                 {(this.state.movieData.belongs_to_collection)?(
                     <div className={"movie-info"} style={{background: `url(http://image.tmdb.org/t/p/original${JSON.parse(this.state.movieData.belongs_to_collection.replace(/'/g, '"')).backdrop_path}) no-repeat center center fixed`}}>
+                        <div className={"movie-info-modal"}>
+                            <div className={"movie-modal-container"}>
+                            <Typography variant="headline" component="h1" className={"font-white font-34em"}>
+                                {this.state.movieData.title}
+                            </Typography>
+                            <div>
+                                <div style={{fontSize: 22}}>
+                                    <span style={{fontSize: 16}}>
+                                    {this.state.movieData.vote_average}
+                                    </span>
+                                    <span>
+                                    /10 <div style={{fontSize: 26}}>
+                                    <StarRatingComponent 
+                                        name="rate1" 
+                                        starCount={5}
+                                        value={(this.state.movieData.vote_average)/2}
+                                        editing={false}
+                                        starColor="#fe1b1b"
+                                        />
+
+                                </div>
+                                    </span>
+                                </div>
+                                <div style={{fontSize: 16}}>
+                                    <span>
+                                    {JSON.parse(this.state.movieData.genres.replace(/'/g, '"')).map(g => g.name.replace(/\ /g,'-')+' ')}
+                                    </span>
+                                </div>
+                                <div className={"movies-desc"}>
+                                    {this.state.movieData.overview}
+                                    <div className={"movies-links"}>
+                                        <Link to="/" className={"movies-href-back"}>Go Back</Link> 
+                                        <a href={`https://www.imdb.com/title/${this.state.movieData.imdb_id}/`} target={"_blank"} className={"movies-href-imdb"}>IMDB Website</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                            </div>
+                            </div>
+                        </div>
                     </div>
                 ):
                 <div className={"movie-info"}>
