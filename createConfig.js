b24322ec7c7ad1a0e53b16e03f2f030eeac38ffb
Arunkumar Palaniappan <11667975+arunkumarpalaniappan@users.noreply.github.com>
2018-05-28 14:52:36 +0530

    typo fix

--- createConfig.js
+++ createConfig.js
@@ -1,14 +1,14 @@
 const fs = require("fs");
 const data = `const serverConfig = {
     "ssl": ${process.env.NODE_SSL},
-    "host": ${process.env.NODE_HOST},
+    "host": "${process.env.NODE_HOST}",
     "port": ${process.env.NODE_PORT}
     },
     elasticSearch = {
         "ssl": ${process.env.ES_SSL},
-        "host": ${process.env.ES_HOST},
+        "host": "${process.env.ES_HOST}",
         "port": ${process.env.ES_PORT},
-        "index": ${process.env.ES_INDEX}
+        "index": "${process.env.ES_INDEX}"
     }
 export {
     serverConfig,
