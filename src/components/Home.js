2d7d7ecf95a6d833762a2f8bb504c420098a4526
Arunkumar Palaniappan <11667975+arunkumarpalaniappan@users.noreply.github.com>
2018-07-30 17:59:19 +0530

    Change ES to Normal Search

--- src/components/Home.js
+++ src/components/Home.js
@@ -144,11 +144,11 @@ class Home extends React.Component {
                 this.setState({alreadyRequested:true});
                 setTimeout(
                     () => { 
-                        this.props.actions.esAutoComplete(this.state.value) 
+                        this.props.actions.search(e.state.value)
                     }, 1000);
             }
             // const searchFun = _.debounce(this.searchwithDebounce, 1000);
-            // this.props.actions.search(e.target.value)
+            // this.props.actions.esAutoComplete(this.state.value) 
         } else {
             this.setState({isSearched : false})
         }
